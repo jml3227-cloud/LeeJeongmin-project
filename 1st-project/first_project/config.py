ed10f1e56d0454ca9c5f7d382a3c91db703c9300
GitHun jml3227 <GitHub jml3227@gmail.com>
2026-04-19 21:15:54 +0900

    Apply dotenv, add .env.example

--- 1st-project/first_project/config.py
+++ 1st-project/first_project/config.py
@@ -1,15 +1,17 @@
 import os
 import oracledb
+from dotenv import load_dotenv
+
+load_dotenv()
 
 oracledb.init_oracle_client(lib_dir=r"C:\oraclexe\instantclient_19_25")
 
 BASE_DIR = os.path.dirname(__file__)
 
-SECRET_KEY = 'dev'
-
-USER = "scott"
-PASSWORD = "tiger"
-DSN = "localhost:1521/xe"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+USER = os.environ.get('DB_USER')
+PASSWORD = os.environ.get('DB_PASSWORD')
+DSN = os.environ.get('DB_DSN')
 
 MODEL_PATH = os.path.join(BASE_DIR, "random_forest.pkl")
 EXPLAINER_PATH = os.path.join(BASE_DIR, "shap_explainer.pkl")

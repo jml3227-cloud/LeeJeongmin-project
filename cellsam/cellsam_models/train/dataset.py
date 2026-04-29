0c0d3009da032ad92941a4643c0b9766487e73c2
jml3227 <jml3227@gmail.com>
2026-04-29 17:08:39 +0900

    add DeepBacs dataset, augmentation, transform

--- cellsam/cellsam_models/train/dataset.py
+++ cellsam/cellsam_models/train/dataset.py
@@ -11,8 +11,10 @@ from sklearn.model_selection import train_test_split
 
 
 class MoNuSACDataset(Dataset):
-    def __init__(self, root_dir, split='train'):
+    def __init__(self, root_dir, split='train', transform=None):
         self.samples = []
+        self.split = split
+        self.transform = transform
         files = os.listdir(root_dir)
         tif_files = sorted([f for f in files if f.endswith('.tif')])
 
@@ -72,6 +74,9 @@ class MoNuSACDataset(Dataset):
 
         masks = resize(masks, [1024, 1024], interpolation=InterpolationMode.NEAREST)
 
+        if self.transform is not None and self.split == 'train':
+            image, boxes, masks = self.transform(image, boxes, masks)
+
         return image, boxes, masks
     
     def parse_xml(self, xml_path, H, W):
@@ -114,8 +119,10 @@ class MoNuSACDataset(Dataset):
         return boxes, masks
     
 class TNBCDataset(Dataset):
-    def __init__(self, root_dir, split='train'):
+    def __init__(self, root_dir, split='train',transform=None):
         self.samples = []
+        self.split = split
+        self.transform = transform
 
         files = os.listdir(root_dir)
         img_files = sorted([f for f in files if f.endswith('.png') and not f.startswith('GT_')])
@@ -176,6 +183,9 @@ class TNBCDataset(Dataset):
 
         masks = resize(masks, [1024, 1024], interpolation=InterpolationMode.NEAREST)
 
+        if self.transform is not None and self.split == 'train':
+            image, boxes, masks = self.transform(image, boxes, masks)
+
         return image, boxes, masks
     
     def parse_mask(self, gt, H, W):
@@ -198,8 +208,10 @@ class TNBCDataset(Dataset):
         return boxes, masks
     
 class NuInsSegDataset(Dataset):
-    def __init__(self, root_dir, split='train'):
+    def __init__(self, root_dir, split='train', transform=None):
         self.samples = []
+        self.split = split
+        self.transform = transform
 
         files = os.listdir(root_dir)
         img_files = sorted([f for f in files if f.startswith('image_') and f.endswith('.png')])
@@ -257,8 +269,11 @@ class NuInsSegDataset(Dataset):
 
         masks = resize(masks, [1024, 1024], interpolation=InterpolationMode.NEAREST)
 
-        return image, boxes, masks
+        if self.transform is not None and self.split == 'train':
+            image, boxes, masks = self.transform(image, boxes, masks)
 
+        return image, boxes, masks
+    
     def parse_mask(self, gt, H, W):
         binary = (gt > 0).astype(np.uint8)
         labeled = label(binary)
@@ -278,8 +293,95 @@ class NuInsSegDataset(Dataset):
 
         return boxes, masks
 
+class DeepBacsDataset(Dataset):
+    def __init__(self, root_dir, split='train', transform=None):
+        self.samples = []
+        self.split = split
+        self.transform = transform
+
+        files = os.listdir(root_dir)
+        img_files = sorted([f for f in files if f.endswith('.tif') and not f.startswith('GT_')])
+
+        for img_file in img_files:
+            gt_file = 'GT_' + img_file
+            if gt_file in files:
+                self.samples.append((
+                    os.path.join(root_dir, img_file),
+                    os.path.join(root_dir, gt_file)
+                ))
         
+        train_samples, temp_samples = train_test_split(self.samples, test_size=0.2, random_state=42)
+        val_samples, test_samples = train_test_split(temp_samples, test_size=0.5, random_state=42)
+
+        if split == 'train':
+            self.samples = train_samples
+        elif split == 'val':
+            self.samples = val_samples
+        elif split == 'test':
+            self.samples = test_samples
+
+    def __len__(self):
+        return len(self.samples)
     
+    def __getitem__(self, idx):
+        img_path, gt_path = self.samples[idx]
+
+        image = np.array(Image.open(img_path).convert('RGB'))
+        gt = np.array(Image.open(gt_path))
+        H, W = image.shape[:2]
+
+        boxes, masks = self.parse_mask(gt, H, W)
+
+        if len(boxes) == 0:
+            return self.__getitem__((idx + 1) % len(self.samples))
+        
+        if len(boxes) > 450:
+            return self.__getitem__((idx + 1) % len(self.samples))
+        
+        image = torch.tensor(image).permute(2, 0, 1).float() / 255.0
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+        masks = torch.tensor(np.array(masks), dtype=torch.uint8)
+
+        image = resize(image, [1024, 1024])
+
+        x_min = boxes[:, 0] / W
+        y_min = boxes[:, 1] / H
+        x_max = boxes[:, 2] / W
+        y_max = boxes[:, 3] / H
+
+        cx = (x_min + x_max) / 2
+        cy = (y_min + y_max) / 2
+        w = x_max - x_min
+        h = y_max - y_min
+
+        boxes = torch.stack([cx, cy, w, h], dim=1)
+
+        masks = resize(masks, [1024, 1024], interpolation=InterpolationMode.NEAREST)
+
+        if self.transform is not None and self.split == 'train':
+            image, boxes, masks = self.transform(image, boxes, masks)
+
+        return image, boxes, masks
+    
+    def parse_mask(self, gt, H, W):
+        labeled = gt.astype(np.int32)
+
+        boxes = []
+        masks = []
+
+        for region in regionprops(labeled):
+            if region.area < 10:
+                continue
+
+            y_min, x_min, y_max, x_max = region.bbox
+
+            boxes.append([x_min, y_min, x_max, y_max])
+            mask = (labeled == region.label).astype(np.uint8)
+            masks.append(mask)
+
+        return boxes, masks
+
+
 def collate_fn(batch):
     images, boxes, masks = zip(*batch)
     images = torch.stack(images)

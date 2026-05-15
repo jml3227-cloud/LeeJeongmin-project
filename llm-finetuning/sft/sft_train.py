aeb0e399e6aee2ff82d7a3b5994109428fd82741
jml3227 <jml3227@gmail.com>
2026-05-15 12:59:09 +0900

    update

--- llm-finetuning/sft/sft_train.py
+++ llm-finetuning/sft/sft_train.py
@@ -10,7 +10,7 @@ from trl import DataCollatorForCompletionOnlyLM
 from datasets import Dataset, load_dataset, concatenate_datasets
 
 CPT_MODEL_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/outputs/cpt"
-DATA_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/data/sft_domain.jsonl"
+DATA_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/data/pubmedqa_filtered.jsonl"
 OUTPUT_DIR = "/workspace/LeeJeongmin-project/llm-finetuning/outputs/sft"
 LOG_DIR = "/workspace/LeeJeongmin-project/llm-finetuning/outputs/logs"
 EPOCHS = 3

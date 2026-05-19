00e74abca99205ad4aa52851b23680e9bb4ecd6b
jml3227 <jml3227@gmail.com>
2026-05-19 10:35:36 +0900

    update

--- llm-finetuning/perplexity.py
+++ llm-finetuning/perplexity.py
@@ -2,7 +2,7 @@ import torch
 import json
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-MODEL_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/outputs/qlora"
+MODEL_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/outputs/qlora_final"
 EVAL_PATH = "/workspace/LeeJeongmin-project/llm-finetuning/data/pubmedqa_ko_eval.jsonl"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)

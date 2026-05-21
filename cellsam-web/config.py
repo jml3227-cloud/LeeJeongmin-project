f6c98567668d4ab243a186dca8fb99d195a0c966
jml3227 <jml3227@gmail.com>
2026-05-21 16:24:18 +0900

    update

--- cellsam-web/config.py
+++ cellsam-web/config.py
@@ -5,8 +5,8 @@ BASE_DIR = os.path.dirname(__file__)
 SECRET_KEY = 'dev'
 
 # Runpod 서버 URL
-CELLSAM_SERVER_URL = os.environ.get('CELLSAM_SERVER_URL', 'http://216.81.245.138:10766') 
-LLM_SERVER_URL = os.environ.get('LLM_SERVER_URL', 'http://216.81.245.138:10766')
+CELLSAM_SERVER_URL = os.environ.get('CELLSAM_SERVER_URL', 'http://216.81.245.125:17582') 
+LLM_SERVER_URL = os.environ.get('LLM_SERVER_URL', 'http://216.81.245.125:17582')
 
 # 업로드 설정
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'app/static/uploads')
